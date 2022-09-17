fc48d8479445f006cd034b013179dfa9ed10e89d
mrdudov <mrdudov@gmail.com>
2022-09-17 12:59:45 +0300

    bugfix

--- traffic_lights_multiple_cars.py
+++ traffic_lights_multiple_cars.py
@@ -3,7 +3,6 @@ def traffic_lights(road, n):
     result = []
     result.append(road)
 
-    road_l = road.split()
     g_tl_live_cycle = 'G'*5 + 'O' + 'R'*5
     o_tl_live_cycle = 'O' + 'R'*5 + 'G'*5
     r_tl_live_cycle = 'R'*5 + 'G'*5 + 'O'
@@ -11,14 +10,14 @@ def traffic_lights(road, n):
     init_tl = [i if i in 'GOR' else 0 for i in road]
 
     tl_n = ['.']*len(road)
-    for m in range(n):
+    for m in range(1, n+1):
         for i, tl in enumerate(init_tl):
             if tl == 'G':
-                tl_n[i] = g_tl_live_cycle[m+1 % 11]
+                tl_n[i] = g_tl_live_cycle[m % 11]
             if tl == 'O':
-                tl_n[i] = o_tl_live_cycle[m+1 % 11]
+                tl_n[i] = o_tl_live_cycle[m % 11]
             if tl == 'R':
-                tl_n[i] = r_tl_live_cycle[m+1 % 11]
+                tl_n[i] = r_tl_live_cycle[m % 11]
         result.append(''.join(tl_n))
 
     return result
