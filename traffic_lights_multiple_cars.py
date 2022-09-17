1f1b4a24423c617991872b388c7955e675aba192
mrdudov <mrdudov@gmail.com>
2022-09-17 17:03:13 +0300

    wip

--- traffic_lights_multiple_cars.py
+++ traffic_lights_multiple_cars.py
@@ -1,23 +1,48 @@
 def traffic_lights(road, n):
-
+    
     result = []
     result.append(road)
-
+    
     g_tl_live_cycle = 'G'*5 + 'O' + 'R'*5
     o_tl_live_cycle = 'O' + 'R'*5 + 'G'*5
     r_tl_live_cycle = 'R'*5 + 'G'*5 + 'O'
-
+    
     init_tl = [i if i in 'GOR' else 0 for i in road]
-
+    
     tl_n = ['.']*len(road)
+    car_n = list([c if c == 'C' else '.' for c in road])
+    
     for m in range(1, n+1):
         for i, tl in enumerate(init_tl):
             if tl == 'G':
-                tl_n[i] = g_tl_live_cycle[m % 11]
+                tl_n[i] = g_tl_live_cycle[m%11]
             if tl == 'O':
-                tl_n[i] = o_tl_live_cycle[m % 11]
+                tl_n[i] = o_tl_live_cycle[m%11]    
             if tl == 'R':
-                tl_n[i] = r_tl_live_cycle[m % 11]
-        result.append(''.join(tl_n))
+                tl_n[i] = r_tl_live_cycle[m%11]
+        
+        
+        for i in range(len(car_n) -1, -1, -1):
+            try:
+                is_tl_g = tl_n[i+1] not in 'OR'
+            except:
+                is_tl_g = True
+            try:
+                is_roar_free = car_n[i+1] == '.'
+            except:
+                is_roar_free = True
+            try:
+                last = tl_n[+1] == 'G' and car_n[i+2] != 'C'
+            except:
+                last = True
 
+            if car_n[i] == 'C' and is_tl_g and is_roar_free and last:
+                try:
+                    car_n[i] = '.'
+                    car_n[i+1] = 'C'
+                except:
+                    car_n[i] = '.'
+        
+        result.append(''.join(car_n))
+    
     return result
