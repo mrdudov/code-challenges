85ae85b21d0c42b841f48fbca19c853108fdb52a
mrdudov <mrdudov@gmail.com>
2022-09-20 17:34:14 +0300

    twice linear add test data

--- twice_linear.py
+++ twice_linear.py
@@ -1,29 +1,44 @@
 from time import time
+import math
+
 
 def dbl_linear(n):
-    def get_y(x):
-        return 2 * x + 1
-
-    def get_z(x):
-        return 3 * x + 1
+    
+    def get_y_z(x):
+        return 2 * x + 1, 3 * x + 1
 
     sequence = [1]
 
     for index in range(n):
-        next_y = get_y(sequence[index])
-        next_z = get_z(sequence[index])
-        sequence.extend([next_y, next_z])
+        sequence.extend(get_y_z(sequence[index]))
         sequence = list(set(sequence))
         sequence.sort()
-
+        # if len(sequence) > n:
+        #     print(sequence)
+        #     break
+    # print(f"{n=} {len(sequence)=}")
     return sequence[n]
-
 
 n = 6000
 
+data = [
+    (10, 22),
+    (20, 57),
+    (30, 91),
+    (50, 175),
+    (100, 447),
+    (500, 3355),
+    (1000, 8488),
+    (2000, 19773),
+    # (6000, 80914),
+    # (60000, 1511311),
+]
+
 t_start = time()
 
-res = dbl_linear(n)
-print(f"{n=} f(n)={res}")
+for i, val in enumerate(data):
+    t_local = time()
+    res = dbl_linear(val[0])
+    print(f"{i+1} {res == val[1]} t={time()-t_local:.3f} n={val[0]} f(n)={res}")
 
-print(time() - t_start)
+print(f"total time: {time() - t_start:.3f}")
