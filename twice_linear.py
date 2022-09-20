9f6a70995d2a631334df2b265a7a8e790dff32a5
mrdudov <mrdudov@gmail.com>
2022-09-20 14:22:54 +0300

    twice linear add time metric

--- twice_linear.py
+++ twice_linear.py
@@ -1,3 +1,5 @@
+from time import time
+
 def dbl_linear(n):
     def get_y(x):
         return 2 * x + 1
@@ -7,16 +9,21 @@ def dbl_linear(n):
 
     sequence = [1]
 
-    # index = 0
-    # while len(sequence) <= n:
     for index in range(n):
         next_y = get_y(sequence[index])
         next_z = get_z(sequence[index])
         sequence.extend([next_y, next_z])
         sequence = list(set(sequence))
         sequence.sort()
-        # index += 1
-    print(sequence)
-    for index, value in enumerate(sequence):
-        print(f"{index:3} - {value:3}")
+
     return sequence[n]
+
+
+n = 6000
+
+t_start = time()
+
+res = dbl_linear(n)
+print(f"{n=} f(n)={res}")
+
+print(time() - t_start)
