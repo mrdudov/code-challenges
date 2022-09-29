f815b86fd11e2190e93db340fc11019cb3b777fe
mrdudov <mrdudov@gmail.com>
2022-09-29 14:30:25 +0300

    instant runoff voiting bugfix

--- instant_runoff_voting.py
+++ instant_runoff_voting.py
@@ -15,15 +15,17 @@ def runoff(voters):
         max_c = Counter([i[0] for i in voters])
         min_c = Counter([i[-1] for i in voters])
         less_candidate = min_c.most_common(1)[0][0]
-        less_candidates = list(filter(lambda x: x == less_candidate , min_c))
+        less_c_val = min_c.most_common(1)[0][1]
+        less_candidates = list(filter(lambda x: x[1] == less_c_val , dict(min_c).items()))
+        print(f"{min_c=}")
         
         pprint(voters)
         print(f"{less_candidate=}")
+        print(f"{less_candidates=}")
         
         if max_c.most_common(1)[0][1] > voters_count / 2:
             return max_c.most_common(1)[0][0]
         if min_c.most_common(1)[0] == max_c.most_common(1)[0]:
-#         if min_c == max_c:
             print(f"{min_c=}")
             print(f"{max_c=}")
             print('return from ==')
@@ -31,6 +33,6 @@ def runoff(voters):
         for candidate in less_candidates:
             for i, vote in enumerate(voters):
                 for j, candidate_in_vote in enumerate(vote):
-                    if candidate_in_vote == candidate:
+                    if candidate_in_vote == candidate[0]:
                         voters[i].pop(j)               
     return None
