3d50409ab0ca5423a25e3dd84dc555d71c666180
mrdudov <mrdudov@gmail.com>
2022-09-29 17:01:53 +0300

    new kata parseint_reloaded

---
+++ parseInt_reloaded.py
@@ -0,0 +1,55 @@
+def parse_int(string):
+    print(string)
+    words = string.split()
+    number = 0
+    
+    ten = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+        "eleven": 11,
+        "twelve": 12,
+        "thirteen": 13,
+        "fourteen": 14,
+        "fifteen": 15,
+        "sixteen": 16,
+        "seventeen": 17,
+        "eighteen": 18,
+        "nineteen": 19,
+        "twenty": 20,
+        "twenty-one": 21,
+        "twenty-two": 22,
+        "twenty-three": 23,
+        "twenty-four": 24,
+        "twenty-five": 25,
+        "twenty-six": 26,
+        "twenty-seven": 27,
+        "twenty-eight": 28,
+        "twenty-nine": 29,
+        "thirty": 30,
+        "thirty-one": 31,
+        "forty": 40,
+        "fifty": 50,
+        "sixty": 60,
+        "seventy": 70,
+        "eighty": 80,
+        "ninety": 90,
+        "one hundred": 100,
+        "five hundred": 500,
+        "one thousand": 1000,
+        "one thousand five hundred": 1500,
+        "one hundred thousand": 100000,
+        "one million": 1000000,
+    }
+    
+    for word in words:
+        if word in ten:
+            number += ten[word]
+    return number
