ccf3e22107c939f8b2d22188923fc11dfbe54db6
mrdudov <mrdudov@gmail.com>
2022-10-01 11:32:22 +0300

    Rail Fence Cipher: Encoding and Decoding Final

--- rail_fence_cipher_encoding_and_decoding.py
+++ rail_fence_cipher_encoding_and_decoding.py
@@ -2,21 +2,31 @@ from itertools import cycle, chain
 
 
 def encode_rail_fence_cipher(string, n):
+
     rails = [[] for _ in range(n)]
+    index = cycle(chain(range(n), range(n-2, 0, -1)))
+    [rails[next(index)].append(character) for character in string]
 
-    c = cycle(chain(range(n), range(n-2, 0, -1)))
-
-    for character in string:
-        rails[next(c)].append(character)
-
-    result = ''
-
-    for rail in rails:
-        result += ''.join(rail)
-
-    return result
+    return ''.join(chain(*rails))
 
 
 def decode_rail_fence_cipher(string, n):
-    rails_len = []
-    return ''
+
+    rails = [[] for _ in range(n)]
+    index = cycle(chain(range(n), range(n-2, 0, -1)))
+    [rails[next(index)].append(character) for character in string]
+
+    line_lens = [len(l) for l in rails]
+    index = cycle(chain(range(n), range(n-2, 0, -1)))
+    rails = []
+
+    prev_idx = 0
+    next_idx = 0
+    for line_len in line_lens:
+        next_idx += line_len
+        rails.append(string[prev_idx:next_idx])
+        prev_idx = next_idx
+
+    rails = [iter(s) for s in rails]
+
+    return ''.join([next(rails[next(index)]) for _ in string])
