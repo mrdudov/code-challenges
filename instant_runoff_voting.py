a5937ceb1e1014bd7bf876bf5df047213ec4eb96
mrdudov <mrdudov@gmail.com>
2022-10-02 17:55:28 +0300

    instant runoff voting WIP

--- instant_runoff_voting.py
+++ instant_runoff_voting.py
@@ -12,7 +12,7 @@ def runoff(voters):
     voters = copy.deepcopy(voters)
     voters_count = len(voters)
     
-    while True:
+    while voters[0]:
         pprint(voters)
 
         # 2. The votes are tallied from the each voter's first choice.
@@ -28,21 +28,33 @@ def runoff(voters):
         
         all_candidate_count = len(voters[0])
         for idx in range(0, math.ceil(all_candidate_count/2)):
+            print("-"*10, "for", "-"*10)
             left_counter = Counter([column[idx] for column in voters])
             right_counter = Counter([column[all_candidate_count - idx - 1] for column in voters])
-            print(f"{left_counter=} {right_counter=}")
+            print(f"{left_counter == right_counter=}")
             print(f"{len(set(right_counter.values()))=}")
+            print(f"{left_counter.most_common(1) == right_counter.most_common(1)=}")
+            
+#             if idx == (math.ceil(all_candidate_count/2)-1) and all_candidate_count%2 != 0:
+#                 print(f"{idx=}")
+        
             if left_counter == right_counter:
                 continue
+                
             if all(map(lambda x : x == 1, right_counter.values())):
                 continue
-#             if len(set(right_counter.values())) != 1:
+            
+            if left_counter.most_common(1) == right_counter.most_common(1):
+                continue
+
             min_candidate, min_candidate_count = right_counter.most_common(1)[0]
+            less_candidates = list(filter(lambda x: x[1] == min_candidate_count , dict(right_counter).items()))
             break
         else:
+            print("from for else")
             return None
         
-        less_candidates = list(filter(lambda x: x[1] == min_candidate_count , dict(right_counter).items()))
+        print(f"{less_candidates=}")
         for candidate in less_candidates:
             for i, vote in enumerate(voters):
                 for j, candidate_in_vote in enumerate(vote):
@@ -50,3 +62,4 @@ def runoff(voters):
                         voters[i].pop(j)
         
         # 6. In case of a complete tie between every candidate, return None
+
