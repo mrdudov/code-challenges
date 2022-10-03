277e2e0854a5de259e6da3e59adbec26b797cf1f
mrdudov <mrdudov@gmail.com>
2022-10-03 21:42:59 +0300

    instant runoff voting pass all tests

--- instant_runoff_voting.py
+++ instant_runoff_voting.py
@@ -3,63 +3,70 @@
 from collections import Counter
 import copy
 import math
+from string import ascii_lowercase
 from pprint import pprint
 
 
 def runoff(voters):
-    # 1. Each voter selects several candidates in order of preference.
 
     voters = copy.deepcopy(voters)
+
+    renamed_flag = False
+
+    if len(voters[0][0]) != 1:
+        renamed_flag = True
+        candidate_iter = iter(voters[0])
+        name_iter = iter(ascii_lowercase)
+        rename_dict = {next(candidate_iter): next(name_iter)
+                       for _ in voters[0]}
+        pprint(rename_dict)
+
+        new_voters = []
+        for voter in voters:
+            new_voters.append([rename_dict[i] for i in voter])
+        voters = new_voters
+
     voters_count = len(voters)
-    
+
     while voters[0]:
+        print("-"*10, " main loop ", "-"*10)
         pprint(voters)
 
-        # 2. The votes are tallied from the each voter's first choice.
-        max_candidate, max_candidate_count = Counter([i[0] for i in voters]).most_common(1)[0]
-        print(f"{max_candidate=}, {max_candidate_count=}")
-        
-        # 3. If the first-place candidate has more than half the total votes, they win.
-        if max_candidate_count > voters_count / 2:
-            return max_candidate
-        
-        # 4. Otherwise, find the candidate who got the least votes and remove them from each person's voting list.
-        # 5. In case of a tie for least, remove all of the tying candidates.
-        
-        all_candidate_count = len(voters[0])
-        for idx in range(0, math.ceil(all_candidate_count/2)):
-            print("-"*10, "for", "-"*10)
-            left_counter = Counter([column[idx] for column in voters])
-            right_counter = Counter([column[all_candidate_count - idx - 1] for column in voters])
-            print(f"{left_counter == right_counter=}")
-            print(f"{len(set(right_counter.values()))=}")
-            print(f"{left_counter.most_common(1) == right_counter.most_common(1)=}")
-            
-#             if idx == (math.ceil(all_candidate_count/2)-1) and all_candidate_count%2 != 0:
-#                 print(f"{idx=}")
-        
-            if left_counter == right_counter:
-                continue
-                
-            if all(map(lambda x : x == 1, right_counter.values())):
-                continue
-            
-            if left_counter.most_common(1) == right_counter.most_common(1):
-                continue
+        max_candidate, max_candidate_count = Counter(
+            [i[0] for i in voters]).most_common(1)[0]
+        max_candidates = list(filter(lambda x: x[1] == max_candidate_count, dict(
+            Counter([column[0] for column in voters])).items()))
+        print(f"{max_candidates=}")
 
-            min_candidate, min_candidate_count = right_counter.most_common(1)[0]
-            less_candidates = list(filter(lambda x: x[1] == min_candidate_count , dict(right_counter).items()))
-            break
-        else:
-            print("from for else")
+        if max_candidate_count > voters_count / 2:
+            print(f"WINER: {max_candidate}")
+            if renamed_flag:
+                return [i[0] for i in rename_dict.items() if i[1] == max_candidate][0]
+            return max_candidate
+
+        if len(set([i[0] for i in voters])) == len(voters):
+            print(f"{set([i[0] for i in voters])=}")
+            print("return None")
             return None
-        
+        if len(set(Counter([i[0] for i in voters]).values())) == 1:
+            return None
+
+        if len(max_candidates) == 1 and len(set(Counter([i[0] for i in voters]).values())) == 2:
+            print(f"WINER: {max_candidate}")
+            if renamed_flag:
+                return [i[0] for i in rename_dict.items() if i[1] == max_candidate][0]
+            return max_candidate
+
+        min_candidate_count = min(
+            Counter([column[0] for column in voters]).values())
+        less_candidates = list(filter(lambda x: x[1] == min_candidate_count, dict(
+            Counter([column[0] for column in voters])).items()))
         print(f"{less_candidates=}")
+
         for candidate in less_candidates:
             for i, vote in enumerate(voters):
                 for j, candidate_in_vote in enumerate(vote):
                     if candidate_in_vote == candidate[0]:
                         voters[i].pop(j)
-        
-        # 6. In case of a complete tie between every candidate, return None
 
+    print(f"WINER: {None}")
