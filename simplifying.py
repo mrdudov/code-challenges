bfdf7abaf305223b3679867d227623b560b6e838
mrdudov <mrdudov@gmail.com>
2022-10-24 16:57:38 +0300

    simplifying WIP

--- simplifying.py
+++ simplifying.py
@@ -1,5 +1,17 @@
+from string import ascii_lowercase
+
 def simplify(examples, formula):
-    return formula[0]
+    
+    d = {}
+    for e in examples:
+        v, k = map(lambda s: s.strip(), e.split('='))
+        d[k] = v
+    result = formula
+    for ch in formula:
+        if ch in ascii_lowercase:
+            result = result.replace(ch, f"({d.get(ch, ch)})")
+    
+    return result
 
 
 examples = [["a + a = b", "b - d = c", "a + b = d"],
@@ -20,6 +32,7 @@ for i in range(len(answer)):
     print('examples:' + str(examples[i]))
     print('formula:' + str(formula[i]))
     print('expected answer:' + str(answer[i]))
-    print(f'{simplify(examples[i], formula[i])=}')
-    print(f'{answer[i]=}')
+    print(f'resut: {simplify(examples[i], formula[i])}')
     print('is test pass: ', simplify(examples[i], formula[i]) == answer[i])
+    break
+
