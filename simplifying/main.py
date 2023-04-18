0baa2e5408c53ea6b8d530c28b7726125a558a96
mrdudov <mrdudov@gmail.com>
2023-04-18 19:50:56 +0300

    simpliofy WIP

--- simplifying/main.py
+++ simplifying/main.py
@@ -3,7 +3,10 @@ from simplifying import simplify
 
 
 for test_case in test_data:
-    print('*'*60)
+    print("*" * 60)
     print(f"equalities: {test_case['equalities']}")
     print(f"formula: {test_case['formula']}")
     print(f"expected answer: {test_case['answer']}")
+    print("-" * 60)
+    result = simplify(test_case["equalities"], test_case["formula"])
+    print(f"{result=}")
--- simplifying/simplifying.py
+++ simplifying/simplifying.py
@@ -2,38 +2,71 @@ import re
 from string import ascii_lowercase
 
 
-def get_equalities(equ):
+def parse_equalities(equ):
+    return dict(map(lambda s: s.strip(), e.split("=")[::-1]) for e in equ)
 
-    return dict(map(lambda s: s.strip(), e.split('=')[::-1]) for e in equ)
+
+def replace_equalities_in_formula(formula, equalities):
+    new_formula = formula
+    old_formula = formula
+
+    while True:
+        old_formula = new_formula
+
+        for key, val in equalities.items():
+            if key in new_formula:
+                new_formula = new_formula.replace(key, f"({val})")
+
+        if new_formula == old_formula:
+            break
+    return new_formula
 
 
-def replace_symbol_to_equality(formula, equalities):
-    while len(set(filter(lambda l: l in ascii_lowercase, formula))) > 1:
-        for ch in formula:
-            if ch in ascii_lowercase and equalities.get(ch, None):
-                formula = formula.replace(ch, f"({equalities[ch]})")
+def remove_whitespace(formula: str) -> str:
+    return formula.replace(" ", "")
 
-    formula = formula.replace(' ', '')
 
+def to_char_add_digit(formula: str) -> str:
+    pattern = r"(?<=[^0-9])[a-z]{1}"
+
+    def add_one(x: re.Match) -> str:
+        if x.group(0):
+            return f"1{x.group(0)}"
+
+    formula = re.sub(pattern=pattern, repl=add_one, string=formula)
+    return formula
+
+
+def open_the_brackets(formula: str) -> str:
+    def calc(match: re.Match) -> str:
+        if match.group(2) == "+":
+            return str(int(match.group(1)) + int(match.group(3))) + match.group(4)
+        if match.group(2) == "-":
+            return str(int(match.group(1)) - int(match.group(3))) + match.group(4)
+
+    while "(" in formula:
+        pattern = r"\(([+-]*[0-9]*)[a-z]([+-])([+-]*[0-9]*)([a-z])\)"
+        formula = re.sub(pattern=pattern, repl=calc, string=formula)
+        print(formula)
+        # break
     return formula
 
 
 def simplify(equalities, formula):
+    equalities = parse_equalities(equalities)
 
-    equalities = get_equalities(equalities)
+    formula = replace_equalities_in_formula(formula=formula, equalities=equalities)
+    formula = remove_whitespace(formula=formula)
 
-    formula = replace_symbol_to_equality(
-        formula=format,
-        equalities=equalities
-    )
+    print("")
+    print(f"remove_whitespace: {formula=}")
 
-    while '(' in formula:
-        r = re.findall(r'\(([^\(]*?)\)', formula)
-        print(r)
+    formula = to_char_add_digit(formula=formula)
 
-        for i in r:
-            print(re.split('\+|-|/|\*', i))
+    print("")
+    print(f"to_char_add_digit: {formula=}")
+    print("")
 
-        break
+    formula = open_the_brackets(formula=formula)
 
     return formula
