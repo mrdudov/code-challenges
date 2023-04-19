e1a16a09ddde44226936e3141b80a446bd915026
mrdudov <mrdudov@gmail.com>
2023-04-20 01:12:35 +0300

    the_millionth_fibonacci add tests

--- the_millionth_fibonacci.py
+++ the_millionth_fibonacci.py
@@ -15,3 +15,27 @@ def fib(n):
 #     if n > 1_000_000:
 #         print(round(a))
     return round(a)
+
+assert fib(0) == 0
+assert fib(1) == 1
+assert fib(2) == 1
+assert fib(3) == 2
+assert fib(4) == 3
+assert fib(5) == 5
+assert fib(-6) == -8
+assert fib(-96) == -51680708854858323072
+assert fib(-33) == 3524578
+assert fib(-20) == -6765
+assert fib(-37) == 24157817
+assert fib(-89) == 1779979416004714189
+assert fib(-86) == -420196140727489673
+assert fib(-37) == 24157817
+assert fib(-60) == -1548008755920
+assert fib(-25) == 75025
+assert fib(-97) == 83621143489848422977
+assert fib(-18) == -2584
+
+# assert fib(1000) == 4E208
+# assert fib(1001) / fib(1000) == (1 + sqrt(5)) / 2
+# assert fib(88119)
+# assert fib(1405306)
