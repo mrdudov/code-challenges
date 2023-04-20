1555536521d01bcaabb85a9b89a0c5d1a7e2b217
mrdudov <mrdudov@gmail.com>
2023-04-20 18:48:32 +0300

    the_millionth_fibonacci all test pass

--- the_millionth_fibonacci.py
+++ the_millionth_fibonacci.py
@@ -2,40 +2,41 @@
 
 import decimal
 import numpy as np
+import gmpy2
 
 def fib(n):
+    if n < 0:
+        decimal.getcontext().prec = 25_000
+        root_5 = decimal.Decimal(5).sqrt()
+        phi = ((1 + root_5) / 2)
+        a = (np.power(phi, n) - np.power(-phi, -n)) / root_5
+        return round(a)
+    return gmpy2.fib(n)
 
 
-    decimal.getcontext().prec = 10000
-
-    root_5 = decimal.Decimal(5).sqrt()
-    phi = ((1 + root_5) / 2)
-
-    a = (np.power(phi, n) - np.power(-phi, -n)) / root_5
-#     if n > 1_000_000:
-#         print(round(a))
-    return round(a)
-
-assert fib(0) == 0
-assert fib(1) == 1
-assert fib(2) == 1
-assert fib(3) == 2
-assert fib(4) == 3
-assert fib(5) == 5
-assert fib(-6) == -8
-assert fib(-96) == -51680708854858323072
-assert fib(-33) == 3524578
-assert fib(-20) == -6765
-assert fib(-37) == 24157817
-assert fib(-89) == 1779979416004714189
-assert fib(-86) == -420196140727489673
-assert fib(-37) == 24157817
-assert fib(-60) == -1548008755920
-assert fib(-25) == 75025
-assert fib(-97) == 83621143489848422977
-assert fib(-18) == -2584
+# assert fib(0) == 0
+# assert fib(1) == 1
+# assert fib(2) == 1
+# assert fib(3) == 2
+# assert fib(4) == 3
+# assert fib(5) == 5
+# assert fib(-6) == -8
+# assert fib(-96) == -51680708854858323072
+# assert fib(-33) == 3524578
+# assert fib(-20) == -6765
+# assert fib(-37) == 24157817
+# assert fib(-89) == 1779979416004714189
+# assert fib(-86) == -420196140727489673
+# assert fib(-37) == 24157817
+# assert fib(-60) == -1548008755920
+# assert fib(-25) == 75025
+# assert fib(-97) == 83621143489848422977
+# assert fib(-18) == -2584
 
 # assert fib(1000) == 4E208
 # assert fib(1001) / fib(1000) == (1 + sqrt(5)) / 2
-# assert fib(88119)
-# assert fib(1405306)
+# print(fib(88119))
+
+# print(fib(1_000_000))
+
+# print(fib(1_405_306))
