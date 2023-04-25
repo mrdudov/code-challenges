99adae9de04e677703a22ba576e5d882df913499
mrdudov <mrdudov@gmail.com>
2023-04-25 13:26:24 +0300

    closest pair improve brute force func use itertools combinations

--- closest_pair/lib.py
+++ closest_pair/lib.py
@@ -1,11 +1,14 @@
 import time
 import functools
 import random
+
 from math import dist
+from itertools import combinations
 
 
 def print_run_time(func):
     """Print the runtime of the decorated function"""
+   
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -17,11 +20,13 @@ def print_run_time(func):
 
 def print_point(point):
     """Print single point"""
+    
     print(f"({point[0]:.3f}, {point[1]:.3f})")
 
 
 def print_points(points):
     """Print points list"""
+    
     print('Points:')
     for point in points:
         print('\t', end='')
@@ -30,6 +35,7 @@ def print_points(points):
 
 def generate_test_points(count:int, min_val = -1_000, max_val = 1_000, seed = 123456):
     """Generate random points"""
+   
     random.seed(seed)
     result = []
     for _ in range(count):
@@ -41,12 +47,12 @@ def generate_test_points(count:int, min_val = -1_000, max_val = 1_000, seed = 12
 
 def closest_pair_brute_force(points):
     """Closest pair brute force"""
+    
     result = (points[0], points[1])
     min_dist = dist(*result)
     
-    for i, point_a in enumerate(points):
-        for j, point_b in enumerate(points):
-            if i != j and dist(point_a, point_b) < min_dist:
-                min_dist = dist(point_a, point_b)
-                result = (point_a, point_b)
+    for pair in combinations(points, r=2):
+        if dist(*pair) < min_dist:
+            min_dist = dist(*pair)
+            result = (pair[0], pair[1])
     return result
