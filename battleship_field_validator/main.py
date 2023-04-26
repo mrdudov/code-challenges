aa931a15d4f4232cbcb89d7b8b0f49d7f6744f04
mrdudov <mrdudov@gmail.com>
2023-04-27 02:00:46 +0300

    battleship field validator add pytest tests

--- battleship_field_validator/main.py
+++ battleship_field_validator/main.py
@@ -1,9 +1,9 @@
-from battleship_field_validator import validate_battlefield
-from data_set import data_set
+from validator import validate_battlefield
+from tests.data_set import data_set
 
 
 if __name__ == '__main__':
-    for test_item in data_set:
-        calc_val = validate_battlefield(test_item['field'])
-        print(f"{calc_val=}")
-        print(f"is correct: {calc_val==test_item['is_valid']}")
+    test_item = data_set[0]
+    calc_val = validate_battlefield(test_item['field'])
+    print(f"{calc_val=}")
+    print(f"is correct: {calc_val==test_item['is_valid']}")
---
+++ battleship_field_validator/tests/test_base.py
@@ -0,0 +1,13 @@
+import pytest
+
+from .data_set import data_set
+from ..validator import validate_battlefield
+
+
+@pytest.mark.parametrize('test_item', data_set)
+def test_battleship_field_validator(test_item):
+    calculated_value = validate_battlefield(test_item["field"])
+    expected_value = test_item["is_valid"]
+    message = test_item["message"]
+
+    assert calculated_value == expected_value, message
