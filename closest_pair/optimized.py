1d3ee97c2afcecc31c5930700e60dc3400202799
mrdudov <mrdudov@gmail.com>
2023-04-26 17:36:03 +0300

    closest pair WIP

--- closest_pair/optimized.py
+++ closest_pair/optimized.py
@@ -32,7 +32,10 @@ def closest_pair_optimized(points, left=0, right=0, is_first_call = True):
         right = data_set_len
         all_points = points   
     
-    if data_set_len < 2:
+    print('')
+    print_points(points)
+
+    if data_set_len <= 1:
         raise ValueError('points count less then two')
         
     if data_set_len == 2:
@@ -41,7 +44,7 @@ def closest_pair_optimized(points, left=0, right=0, is_first_call = True):
     if data_set_len == 3:
         result = min_pair(points)
     
-    if data_set_len > 3:
+    if data_set_len >= 4:
 
         left_left = left
         left_right = (right+left)//2
@@ -66,26 +69,36 @@ def closest_pair_optimized(points, left=0, right=0, is_first_call = True):
             is_first_call=False
         )
         
-        result = left_result if dist(*left_result) < dist(*right_result) else right_result
+        # result = left_result if dist(*left_result) < dist(*right_result) else right_result
+        result = min_pair(left_result + right_result)
 
         s_left = []
-        for point in all_points[left::-1]:
+        left_coordinate = left - 1 if left >= 1 else 0
+        for point in all_points[left_coordinate::-1]:
             if all_points[left][0] - point[0] <= dist(*result):
                 s_left.append(point)
             else:
                 break
 
         s_right = []
-        for point in all_points[right:]:
+        for point in all_points[right+1:]:
             if point[0] - all_points[right][0] <= dist(*result):
                 s_right.append(point)
             else:
                 break
 
-        s = s_left + s_right
+        s = sorted(s_left + s_right, key=lambda p : p[0])
 
         if len(s) >= 2:
             s_min = min_pair(s)
-            result = result if dist(*result) < dist(*s_min) else s_min
+            print('-'*40)
+            print_points(s)
+            print(f"{s_min=}")
+            print('-'*40)
+            # result = result if dist(*result) < dist(*s_min) else s_min
+            print(f"{result=}, {s=}")
+            result = min_pair(list(result) + s)
+
+    print(f"{result=}")
 
     return result
