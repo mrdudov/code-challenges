6cf94059a63aad827ce10b51dcb4686633ebd166
mrdudov <mrdudov@gmail.com>
2023-04-28 11:54:23 +0300

    instant runoff voting remove print statements

--- instant_runoff_voting/voting.py
+++ instant_runoff_voting/voting.py
@@ -1,7 +1,6 @@
-from collections import Counter
 import copy
+from collections import Counter
 from string import ascii_lowercase
-from pprint import pprint
 
 
 def runoff(voters):
@@ -16,7 +15,6 @@ def runoff(voters):
         name_iter = iter(ascii_lowercase)
         rename_dict = {next(candidate_iter): next(name_iter)
                        for _ in voters[0]}
-        pprint(rename_dict)
 
         new_voters = []
         for voter in voters:
@@ -26,30 +24,23 @@ def runoff(voters):
     voters_count = len(voters)
 
     while voters[0]:
-        print("-"*10, " main loop ", "-"*10)
-        pprint(voters)
 
         max_candidate, max_candidate_count = Counter(
             [i[0] for i in voters]).most_common(1)[0]
         max_candidates = list(filter(lambda x: x[1] == max_candidate_count, dict(
             Counter([column[0] for column in voters])).items()))
-        print(f"{max_candidates=}")
 
         if max_candidate_count > voters_count / 2:
-            print(f"WINER: {max_candidate}")
             if renamed_flag:
                 return [i[0] for i in rename_dict.items() if i[1] == max_candidate][0]
             return max_candidate
 
         if len(set([i[0] for i in voters])) == len(voters):
-            print(f"{set([i[0] for i in voters])=}")
-            print("return None")
             return None
         if len(set(Counter([i[0] for i in voters]).values())) == 1:
             return None
 
         if len(max_candidates) == 1 and len(set(Counter([i[0] for i in voters]).values())) == 2:
-            print(f"WINER: {max_candidate}")
             if renamed_flag:
                 return [i[0] for i in rename_dict.items() if i[1] == max_candidate][0]
             return max_candidate
@@ -58,12 +49,9 @@ def runoff(voters):
             Counter([column[0] for column in voters]).values())
         less_candidates = list(filter(lambda x: x[1] == min_candidate_count, dict(
             Counter([column[0] for column in voters])).items()))
-        print(f"{less_candidates=}")
 
         for candidate in less_candidates:
             for i, vote in enumerate(voters):
                 for j, candidate_in_vote in enumerate(vote):
                     if candidate_in_vote == candidate[0]:
                         voters[i].pop(j)
-
-    print(f"WINER: {None}")
