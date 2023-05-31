5f0c31843ea77d5a3feff91a1dccffb7c8f78d2d
mrdudov <mrdudov@gmail.com>
2023-06-01 00:27:07 +0300

    frontend-mentor interactive card details add cards component

---
+++ frontend_mentor/solutions/junior/interactive-card-details-form-main/src/components/cards/cards.component.js
@@ -0,0 +1,13 @@
+export { CardsComponent }
+
+import html from "./cards.component.html"
+import './cards.component.css'
+
+
+class CardsComponent {
+  constructor() {}
+  get_html() {
+    return html
+  }
+  after_dom() {}
+}
--- frontend_mentor/solutions/junior/interactive-card-details-form-main/src/main.js
+++ frontend_mentor/solutions/junior/interactive-card-details-form-main/src/main.js
@@ -3,14 +3,20 @@ import "./styles/style.css"
 
 import { FormComponent } from "./components/form-component/form.component.js"
 import { CompleteComponent } from "./components/complete/complete.component.js"
+import { CardsComponent } from "./components/cards/cards.component.js"
 
 const form_component = new FormComponent()
 const complete_component = new CompleteComponent()
+const cards_component = new CardsComponent()
 
 const form_area = document.querySelector(".form-area")
+const img_area = document.querySelector(".img-area")
+
 
 form_area.innerHTML += form_component.get_html()
 form_area.innerHTML += complete_component.get_html()
+img_area.innerHTML = cards_component.get_html()
 
 form_component.after_dom()
 complete_component.after_dom()
+cards_component.after_dom()
