d37288c4cdde2165e64ba54cfc93c6887ff8d930
mrdudov <mrdudov@gmail.com>
2023-09-24 12:23:21 +0300

    add webpack configuration

---
+++ frontend_mentor/solutions/junior/expenses-chart-component-main/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./main.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      favicon: "./assets/images/favicon-32x32.png",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|img)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+}
