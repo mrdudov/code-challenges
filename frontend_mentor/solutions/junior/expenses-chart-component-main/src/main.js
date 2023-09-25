0e00e945718f09ed6c31bbdfa79165903311bcea
mrdudov <mrdudov@gmail.com>
2023-09-25 17:39:47 +0300

    FM expenses-chart-component base layout complete

--- frontend_mentor/solutions/junior/expenses-chart-component-main/src/main.js
+++ frontend_mentor/solutions/junior/expenses-chart-component-main/src/main.js
@@ -1,1 +1,19 @@
-console.log('main.js')
+import "./styles/styles.css"
+
+import { data } from "./data.js"
+
+const chart = document.querySelector(".chart")
+
+const max = Math.max(...data.map((item) => item.amount))
+
+data.forEach((day) => {
+  let height = (100 * day.amount) / max
+  chart.innerHTML += `
+        <div class="chart-item">
+            <div class="${
+              day.amount === max ? "chart-item-max-column" : "chart-item-column"
+            }" style="height:${height}%;"></div>
+            <p class="cart-title">${day["day"]}</p>
+        </div>
+    `
+})
